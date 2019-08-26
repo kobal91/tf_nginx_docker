98cc58a30c069c725f06e3e1dd550c490902814c
kobal91 <kobal91@gmail.com>
2019-08-26 15:58:40 +0200

    Change docker config.tf settings.

--- config.tf
+++ config.tf
@@ -1,8 +1,5 @@
 provider "docker" {
   host = "tcp://10.0.75.1:2375"
-  #host = "tcp://10.0.75.2:2375"
-  #host = "tcp://localhost:2375"
-  #host = "tcp://docker:2375"
   }
 
 resource "docker_image" "centos" {
@@ -12,4 +9,5 @@ resource "docker_image" "centos" {
 resource "docker_container" "centos-server" {
   image = "${docker_image.centos.latest}"
   name = "centos-server"
+  start = true
 }
